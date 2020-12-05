135631138a6aff8e075b17ee097facf9bcee8b06
NoriakiOtsuka <carly13rae7jepson@gmail.com>
2020-12-05 19:22:26 +0900

    Studentテーブル カラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_05_093137) do
+ActiveRecord::Schema.define(version: 2020_12_05_094242) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -32,6 +32,21 @@ ActiveRecord::Schema.define(version: 2020_12_05_093137) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "kana_name"
+    t.string "parent_name"
+    t.integer "gender"
+    t.date "birthday"
+    t.string "school"
+    t.integer "grade"
+    t.integer "prefecture"
+    t.text "address"
+    t.string "phone"
+    t.string "parent_email"
+    t.integer "class"
+    t.integer "membership_number"
+    t.boolean "status", default: true, null: false
     t.index ["email"], name: "index_students_on_email", unique: true
     t.index ["reset_password_token"], name: "index_students_on_reset_password_token", unique: true
   end
