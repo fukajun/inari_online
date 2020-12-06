7fd579144193ff9f4b3f95d6a86a73c487ecedcd
NoriakiOtsuka <carly13rae7jepson@gmail.com>
2020-12-06 12:09:26 +0900

    Sign_up, Sign_in 項目追加 (プルダウン除く)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :students
+  get 'homes/top'
+  get 'homes/about'
   devise_for :admins
+
+  devise_for :students, controllers:{
+  	sessions:      "students/sessions",
+		passwords:     "students/passwords",
+		registrations: "students/registrations" 
+  }
+  root to: 'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
