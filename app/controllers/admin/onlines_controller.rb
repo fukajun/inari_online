83c4427946bb2a515a2483fa1f244f839a304db3
NoriakiOtsuka <carly13rae7jepson@gmail.com>
2020-12-13 15:59:17 +0900

    Admin::online 表示機能追記

--- app/controllers/admin/onlines_controller.rb
+++ app/controllers/admin/onlines_controller.rb
@@ -1,10 +1,27 @@
 class Admin::OnlinesController < ApplicationController
-  def index
-  end
+	def index
+		@onlines = Online.all
+	end
 
-  def show
-  end
+	def show
+		@online = Online.find(params[:id])
+	end
 
-  def edit
-  end
+	def edit
+		@online = Online.find(params[:id])
+	end
+
+	def update
+		@online = Online.find(params[:id])
+		if @online.update(online_params)
+			render "show", notice: "successfully updated!"
+		else
+			render "edit"
+		end
+	end
+
+	private
+	def online_params
+		params.require(:online).permit(:first_name, :last_name, :kana_name, :parent_name, :gender, :birthday, :school, :grade, :prefecture, :address, :phone, :parent_email, :subject)
+	end
 end
