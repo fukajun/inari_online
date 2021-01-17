5e073d485fa1e19d3e5a259f7707f0c3738324b9
NoriakiOtsuka <carly13rae7jepson@gmail.com>
2021-01-17 18:29:48 +0900

    新規登録 登録完了画面URLid付与

--- config/routes.rb
+++ config/routes.rb
@@ -30,7 +30,7 @@ Rails.application.routes.draw do
 
   devise_scope :online do
     post "onlines/sign_up/confirm", to: "onlines/registrations#confirm"
-    get "onlines/sign_up/complete", to: "onlines/registrations#complete"
+    get "onlines/sign_up/complete/:id", to: "onlines/registrations#complete", as: "onlines_sign_up_complete"
   end
 
   resources :onlines, only: [:show, :edit, :update]
