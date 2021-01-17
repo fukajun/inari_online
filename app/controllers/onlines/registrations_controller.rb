066241b1f14ef3e97a57896948d995e9894c7c35
NoriakiOtsuka <carly13rae7jepson@gmail.com>
2021-01-17 18:28:57 +0900

    コード整理

--- app/controllers/onlines/registrations_controller.rb
+++ app/controllers/onlines/registrations_controller.rb
@@ -2,29 +2,37 @@
 
 class Onlines::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create, :confirm]
-  before_action :configure_account_update_params, only: [:update]
+  # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
   # def new
   #   super
   # end
 
-  # POST /resource
-  def create
-    @online = Online.new(sign_up_params)
-    render :new and return if params[:back]
-    super
-  end
-
   def confirm
     @online = Online.new(sign_up_params)
     @online.password = Devise.friendly_token.first(8) #パスワード自動生成
+
     if @online.valid?
       render :action => "confirm"
     else
       render :action => "new"
     end
+  end
 
+  # POST /resource
+  def create
+    @online = Online.new(sign_up_params)
+
+    if params[:back] #登録画面に戻る
+      render :new
+      return
+    else #完了画面に進む
+      @online.save
+      redirect_to onlines_sign_up_complete_path(@online)
+    end
+
+  #   super
   end
 
   def complete
@@ -54,7 +62,7 @@ class Onlines::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
@@ -62,15 +70,14 @@ class Onlines::RegistrationsController < Devise::RegistrationsController
   end
 
   # If you have extra params to permit, append them to the sanitizer.
-  def configure_account_update_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :kana_name, :parent_name, :gender, :birthday, :school, :grade, :prefecture, :address, :phone, :email, :parent_email, :subject, :membership_number, :status])
-  end
+  # def configure_account_update_params
+  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
+  # end
 
   # The path used after sign up.
-  def after_sign_up_path_for(resource)
-    onlines_sign_up_complete_path(resource)
-    # super(resource)
-  end
+  # def after_sign_up_path_for(resource)
+  #   super(resource)
+  # end
 
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
