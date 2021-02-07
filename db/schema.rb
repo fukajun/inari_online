3d0a8c00c1ef880fdb505dd0c6ea59d878f5ad9c
NoriakiOtsuka <carly13rae7jepson@gmail.com>
2021-02-07 10:35:50 +0900

    Questionテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_30_124309) do
+ActiveRecord::Schema.define(version: 2021_02_06_093453) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -50,6 +50,12 @@ ActiveRecord::Schema.define(version: 2021_01_30_124309) do
     t.boolean "status", default: true, null: false
     t.index ["email"], name: "index_onlines_on_email", unique: true
     t.index ["reset_password_token"], name: "index_onlines_on_reset_password_token", unique: true
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "subjects", force: :cascade do |t|
