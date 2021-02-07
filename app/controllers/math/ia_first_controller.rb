49984fe31b171e0fed4b35c13d28d334a7d551c6
NoriakiOtsuka <carly13rae7jepson@gmail.com>
2021-02-07 21:36:22 +0900

    Online 数IA画像表示

--- app/controllers/math/ia_first_controller.rb
+++ app/controllers/math/ia_first_controller.rb
@@ -4,14 +4,26 @@ class Math::IaFirstController < ApplicationController
 	end
 
 	def test
+		parameter = params[:id].to_i
+		@question =Question.where("title like ?", "math_iaf_test_#{parameter}%").where.not("title like ?", "%_answer%").order(title: "ASC")
+		@id = @question[0].title.delete!("math_iaf_test_")
 	end
 
 	def test_answer
+		parameter = params[:id].to_i
+		@question =Question.where("title like ?", "math_iaf_test_answer_#{parameter}%").order(title: "ASC")
+		@id = @question[0].title.delete!("math_iaf_test_answer_")
 	end
 
 	def exercise
+		parameter = params[:id].to_i
+		@question =Question.where("title like ?", "math_iaf_exercise_#{parameter}%").where.not("title like ?", "%_answer%").order(title: "ASC")
+		@id = @question[0].title.delete!("math_iaf_exercise_")
 	end
 
 	def exercise_answer
+		parameter = params[:id].to_i
+		@question =Question.where("title like ?", "math_iaf_exercise_answer_#{parameter}%").order(title: "ASC")
+		@id = @question[0].title.delete!("math_iaf_exercise_answer_")
 	end
 end
