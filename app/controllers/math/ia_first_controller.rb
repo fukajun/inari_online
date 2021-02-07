7400d7aec7add001e2ee3304f154c9eb06e6229d
NoriakiOtsuka <carly13rae7jepson@gmail.com>
2021-02-07 22:41:57 +0900

    Online 数IA複数画像表示

--- app/controllers/math/ia_first_controller.rb
+++ app/controllers/math/ia_first_controller.rb
@@ -6,24 +6,44 @@ class Math::IaFirstController < ApplicationController
 	def test
 		parameter = params[:id].to_i
 		@question =Question.where("title like ?", "math_iaf_test_#{parameter}%").where.not("title like ?", "%_answer%").order(title: "ASC")
-		@id = @question[0].title.delete!("math_iaf_test_")
+
+		@id = Array.new
+		@question.each do |i|
+			num = i.title.delete!("math_iaf_test_")
+			@id.push(num)
+		end
 	end
 
 	def test_answer
 		parameter = params[:id].to_i
 		@question =Question.where("title like ?", "math_iaf_test_answer_#{parameter}%").order(title: "ASC")
-		@id = @question[0].title.delete!("math_iaf_test_answer_")
+
+		@id = Array.new
+		@question.each do |i|
+			num = i.title.delete!("math_iaf_test_answer_")
+			@id.push(num)
+		end
 	end
 
 	def exercise
 		parameter = params[:id].to_i
 		@question =Question.where("title like ?", "math_iaf_exercise_#{parameter}%").where.not("title like ?", "%_answer%").order(title: "ASC")
-		@id = @question[0].title.delete!("math_iaf_exercise_")
+
+		@id = Array.new
+		@question.each do |i|
+			num = i.title.delete!("math_iaf_exercise_")
+			@id.push(num)
+		end
 	end
 
 	def exercise_answer
 		parameter = params[:id].to_i
 		@question =Question.where("title like ?", "math_iaf_exercise_answer_#{parameter}%").order(title: "ASC")
-		@id = @question[0].title.delete!("math_iaf_exercise_answer_")
+
+		@id = Array.new
+		@question.each do |i|
+			num = i.title.delete!("math_iaf_exercise_answer_")
+			@id.push(num)
+		end
 	end
 end
