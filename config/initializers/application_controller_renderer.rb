59b86a0698f8124479d185d6d4fd071e5ebc7304
NoriakiOtsuka <carly13rae7jepson@gmail.com>
2021-02-20 10:09:17 +0900

    Refile secret_key 設定

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '1016d1f5bb93b75c30fa28c09fcbaa1c3af64f6809c521bf2e9ab94f0cc537783cf316e24286b3b29dc799d639228e52d5a60876ebd81b21d724da7f506a9c42'
