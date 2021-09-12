cdb38078bfc64b75f3c04ced35ae3418081bfa9e
NoriakiOtsuka <carly13rae7jepson@gmail.com>
2021-09-12 10:45:00 +0900

    Parent:session ログイン機能追加

--- app/controllers/parents/sessions_controller.rb
+++ app/controllers/parents/sessions_controller.rb
@@ -3,8 +3,22 @@ class Parents::SessionsController < ApplicationController
 	end
 
 	def create
+		parent = Online.find_by!(membership_number: session_params[:membership_number])
+		if parent.parent_email == session_params[:parent_email]
+			parent_sign_in parent
+			redirect_to reports_path
+		else
+			render 'new'
+		end
 	end
 
 	def destroy
+		parent_sign_out
+		redirect_to new_parent_session_path
+	end
+
+	private
+	def session_params
+		params.require(:session).permit(:membership_number, :parent_email)
 	end
 end
--- app/helpers/parents/sessions_helper.rb
+++ app/helpers/parents/sessions_helper.rb
@@ -1,2 +1,18 @@
 module Parents::SessionsHelper
+	def parent_sign_in(parent)
+		session[:parent_id] = parent.id
+	end
+
+	def current_parent
+		@current_parent ||= Online.find_by(id: session[:parent_id])
+	end
+
+	def parent_signed_in?
+		!current_parent.nil?
+	end
+
+	def parent_sign_out
+		session.delete(:parent_id)
+		@current_parent = nil
+	end
 end
