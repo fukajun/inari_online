b2a5c08d605376fe98364ef035e4f94d3f8cb0d6
NoriakiOtsuka <carly13rae7jepson@gmail.com>
2021-09-12 11:08:34 +0900

    Parent::report 学習記録表示

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,4 +1,12 @@
 class ReportsController < ApplicationController
+	before_action :authenticate_parent!
+
 	def index
+		@studies = Study.where(online_id: current_parent.id)
+	end
+
+	private
+	def authenticate_parent!
+		redirect_to new_parent_session_path unless parent_signed_in?
 	end
 end
