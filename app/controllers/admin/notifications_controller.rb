5d99476b4e9130640d944b1e8040467737818224
NoriakiOtsuka <carly13rae7jepson@gmail.com>
2022-02-18 10:24:58 +0900

    Admin::notification レイアウト修正

--- app/controllers/admin/notifications_controller.rb
+++ app/controllers/admin/notifications_controller.rb
@@ -1,46 +1,47 @@
 class Admin::NotificationsController < ApplicationController
-	before_action :authenticate_admin!
-	
-	def index
-		@notifications = Notification.all.order(id: "DESC")
-	end
+  before_action :authenticate_admin!
 
-	def new
-		@notification = Notification.new
+  def index
+    @notifications = Notification.all.order(id: "DESC").page(params[:page])
+  end
 
-		@q = Online.ransack(params[:q])
-		@onlines = @q.result(distinct: true)
-	end
+  def new
+    @notification = Notification.new
+    @courses = Online.courses.keys
 
-	def create
-		@notification = Notification.new(notification_params)
-		if @notification.save
-			onlineArray = params[:notification][:onlineList].split(",")
-			for num in onlineArray do
-				@notification_history = NotificationHistory.new
-				@notification_history.online_id = num
-				@notification_history.notification_id = @notification.id
-				@notification_history.save
-			end
-			redirect_to admin_notifications_path
-		else
-			render :new
-		end
-	end
+    @q = Online.ransack(params[:q])
+    @onlines = @q.result(distinct: true)
+  end
 
-	def show
-		@notification_histories = NotificationHistory.where(notification_id: params[:id])
-		@notification = Notification.find(params[:id])
-	end
+  def create
+    @notification = Notification.new(notification_params)
+    if @notification.save
+      onlineArray = params[:notification][:onlineList].split(",")
+      for num in onlineArray do
+        @notification_history = NotificationHistory.new
+        @notification_history.online_id = num
+        @notification_history.notification_id = @notification.id
+        @notification_history.save
+      end
+      redirect_to admin_notifications_path
+    else
+      render :new
+    end
+  end
 
-	def destroy
-		@notification = Notification.find(params[:id])
-		@notification.destroy
-		redirect_to request.referer
-	end
+  def show
+    @notification_histories = NotificationHistory.where(notification_id: params[:id])
+    @notification = Notification.find(params[:id])
+  end
 
-	private
-	def notification_params
-		params.require(:notification).permit(:title, :body)
-	end
+  def destroy
+    @notification = Notification.find(params[:id])
+    @notification.destroy
+    redirect_to request.referer
+  end
+
+  private
+  def notification_params
+    params.require(:notification).permit(:title, :body)
+  end
 end
